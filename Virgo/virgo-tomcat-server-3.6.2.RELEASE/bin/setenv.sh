ad5bf53a3af95997892b95469aa6dde2002d80aa
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-07-16 18:51:09 +0200

    [TAC-125] comment yourkit env

--- Virgo/virgo-tomcat-server-3.6.2.RELEASE/bin/setenv.sh
+++ Virgo/virgo-tomcat-server-3.6.2.RELEASE/bin/setenv.sh
@@ -1,3 +1,3 @@
-YJP_HOME=/home/mffrench/bin/yjp-10.0.6
-JAVA_OPTS="-agentpath:$YJP_HOME/bin/linux-x86-64/libyjpagent.so=disablestacktelemetry,disableexceptiontelemetry,builtinprobes=none,delay=1000"
+#YJP_HOME=/home/mffrench/bin/yjp-10.0.6
+#JAVA_OPTS="-agentpath:$YJP_HOME/bin/linux-x86-64/libyjpagent.so=disablestacktelemetry,disableexceptiontelemetry,builtinprobes=none,delay=1000"
 
