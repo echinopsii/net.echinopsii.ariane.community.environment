de65c3ace273406c455d6c3321c5acb03669afe2
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-09-11 10:37:23 +0200

    [TAC-150] ariane-scenarios-tradeworflow cookbook init

---
+++ Chef/cookbooks/ariane-scenarios-tradeworkflow/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: ariane-scenarios-tradeworkflow
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "rabbitmq"
+
+
+
