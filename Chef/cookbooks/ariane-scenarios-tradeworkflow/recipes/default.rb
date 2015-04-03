3b8cdf9e726f108bcd86d9584352d0be3392bb36
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2015-04-03 15:35:43 +0200

    [ACC-21] clean environment

--- Chef/cookbooks/ariane-scenarios-tradeworkflow/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: ariane-scenarios-tradeworkflow
-# Recipe:: default
-#
-# Copyright 2014, echinopsii
-#
-# All rights reserved - AGPLv3 licensed
-#
-
--- Docker/tools/cleanDocker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker rm $(docker ps -a -q)
-docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
