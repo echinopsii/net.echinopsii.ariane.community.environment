2f573d5c21125b6c57a880c6346f61d99ed937d3
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-10-09 14:46:42 +0200

    [ACC-286] sync minified taitale files

--- Virgo/virgo-tomcat-server-3.6.2.RELEASE/bin/syncwebdev.sh
+++ Virgo/virgo-tomcat-server-3.6.2.RELEASE/bin/syncwebdev.sh
@@ -33,8 +33,17 @@ rm -rf $KERNEL_HOME/ariane/static/fonts/*
 rm -rf $KERNEL_HOME/ariane/static/images/*
 rm -rf $KERNEL_HOME/ariane/static/js/*
 
-cp -R $ARIANE_HOME/ariane.community.core.portal/wresources/ariane/static/* $KERNEL_HOME/ariane/static/
-cp -R $ARIANE_HOME/ariane.community.core.mapping/taitale/ariane/static/* $KERNEL_HOME/ariane/static/
+cp -R $ARIANE_HOME/ariane.community.core.portal/wresources/ariane/static/ $KERNEL_HOME/ariane/static/
+cp -R $ARIANE_HOME/ariane.community.core.mapping/taitale/src/main/webapp/ariane/static/* $KERNEL_HOME/ariane/static/
+
+ls $ARIANE_HOME/ariane.community.core.mapping/taitale/target/*jar > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	rm $ARIANE_HOME/ariane.community.core.mapping/taitale/target/*jar
+fi
+minifyed_taitale_target=`ls -d $ARIANE_HOME/ariane.community.core.mapping/taitale/target/net* 2>/dev/null`
+if [ $? -eq 0 ]; then
+	cp -R $minifyed_taitale_target/ariane/static/* $KERNEL_HOME/ariane/static/
+fi
 
 list_xhtml_files=`find $KERNEL_HOME -name "*xhtml" | grep "net.echinopsii"` 
 
